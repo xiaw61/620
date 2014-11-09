ada2da18eeba99d5c8d2ba9a0d560bd6d45c21e8
xiaw <xiaw@h-ea40.lab8.csi.miamioh.edu>
2014-11-09 11:09:43 -0500

    need to fix the kernal

--- hw7/hw7.cpp
+++ hw7/hw7.cpp
@@ -28,6 +28,7 @@ std::vector<int> indexCharList(std::vector<char> charList) {
         index++;
         if(ch == NULL)ret.push_back(index);
     }
+    ret.pop_back();
     return ret;
 }
 void  putStringRight(std::vector<int> a, std::vector<int> b,
@@ -43,15 +44,63 @@ void  putStringRight(std::vector<int> a, std::vector<int> b,
     }
 
 }
-void wirte2File(std::vector<char> charList,std::vector<int> index,
+void wirte2File(char * charList,int size,
                 std::string fileName) {
     
     std::ofstream outFile(fileName);
-    for(int i = 0; i < index.size();i++){
-       
-        std::string str( &(charList[index[i]]) );
-        outFile <<str<<'\n';       
+    int start = 0;
+    std::string str( &(charList[start]) );
+    outFile <<str<<'\n';
+    for(int i = 0; i < size;i++){
+        if(charList[i] == NULL) {
+            start = i;
+            if( i != size-1) {
+            std::string str( &(charList[start+1]) );
+            outFile <<str<<'\n';
+            }
+        }
     }
+}
+std::vector<int> func(std::vector<char> charList,cl::KernelFunctor& sort,
+                      std::vector<int> index, ClCmdQueue& queue) throw(cl::Error) {
+    std::vector<int> ret(index.size());
+
+    cl::Buffer charBuf = queue.makeBuffer(  const_cast<char *> (&charList[0]), charList.size(), queue.ROFlags);
+    cl::Buffer indexBuf = queue.makeBuffer(  const_cast<int *>(&index[0]),index.size()*sizeof(int),queue.ROFlags);
+
+    cl::Buffer retBuf = queue.makeBuffer( const_cast<int *> (&ret[0]),
+                                           index.size()*sizeof(int), queue.RWFlags);
+    int size = index.size();
+
+    sort(charBuf,indexBuf,size);
+    queue.enqueueMapBuffer(retBuf, index.size()*sizeof(int));
+    
+
+    return ret;    
+}
+int getDevType(const std::string& device) throw(std::runtime_error) {
+    const std::string DevTypes = "CPUGPUACC";
+    switch (DevTypes.find(device)) {
+    case 0: return CL_DEVICE_TYPE_CPU;
+    case 3: return CL_DEVICE_TYPE_GPU;
+    case 6: return CL_DEVICE_TYPE_ACCELERATOR;
+    }
+    throw std::runtime_error("Invalid device type specified");
+}
+std::vector<int> sortedIndex(std::vector<char> charList,
+                             std::vector<int> index, std::string unit) {
+    std::vector<int> ret;
+    int temp = getDevType(unit);
+    ClCmdQueue queue(temp);
+
+    cl::Kernel kernel   = queue.loadKernel("hw7.cl", "sort");
+    //const int blockSize = 8192;
+    int range = index.size();
+    cl::KernelFunctor sort(kernel, queue.getQueue(), cl::NullRange,
+                             cl::NDRange(range), cl::NullRange);
+    ret = func(charList,sort,index,queue);
+
+    return ret;
 }
 
 int main(int argc, char *argv[]) {
@@ -62,6 +111,13 @@ int main(int argc, char *argv[]) {
     }
     std::vector<char> charList = readData(argv[2]);
     std::vector<int> index = indexCharList(charList);
-    wirte2File(charList,index,argv[3]);
+    char ret[charList.size()];
+    
+    //    std::vector<int> index2={7,4,0};
+    std::vector<int> index2=sortedIndex(charList,index,argv[1]);
+    /*
+    putStringRight(index,index2,charList,ret);
+    wirte2File(ret,charList.size(),argv[3]);
+    */
 
 }
