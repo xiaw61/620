40f16048ecf5c031c08293c924f0e3d8caeec0f0
xiaw <xiaw@h-ea40.lab8.csi.miamioh.edu>
2014-11-20 19:59:35 -0500

    hw8

--- hw7/hw7.cpp
+++ hw7/hw7.cpp
@@ -1,82 +1,81 @@
+// @copyright xiaw
+
 #include <stdexcept>
 #include <string>
-#include "ClCmdQueue.h"
 #include <vector>
-#include <string>
+#include <algorithm>
+#include "ClCmdQueue.h"
 std::vector<char> readData(const std::string& fileName)
-    throw(std::runtime_error){
+    throw(std::runtime_error) {
     std::vector<char> ret;
     std::ifstream file(fileName);
     if (!file.good()) {
         throw std::runtime_error("Error opening input file.");
     }
     std::string temp;
-    while(file >> temp) {
-        for(char ch: temp) {
+    while (file >> temp) {
+        for (char ch : temp) {
             ret.push_back(ch);
         }
-        ret.push_back(NULL);
-
+        ret.push_back('\0');
     }
-    return ret;    
+    return ret;
 }
 std::vector<int> indexCharList(std::vector<char> charList) {
     std::vector<int> ret;
     ret.push_back(0);
     int index = 0;
-    for(char ch:charList) {
+    for (char ch : charList) {
         index++;
-        if(ch == NULL)ret.push_back(index);
+        if (ch == '\0')ret.push_back(index);
     }
     ret.pop_back();
     return ret;
 }
 void  putStringRight(std::vector<int> a, std::vector<int> b,
-                                 std::vector<char> & charList,char *ret) {
-
-    for(decltype(a.size()) i = 0; i < a.size(); i++) {
+                                 std::vector<char> & charList, char *ret) {
+    for (decltype(a.size()) i = 0; i < a.size(); i++) {
         int m = a[i];
         int n = b[i];
-        while(charList[m] != NULL) {
+        while (charList[m] != '\0') {
             ret[n++] =  charList[m++];
         }
-        ret[n] = NULL;
+        ret[n] = '\0';
     }
-
 }
-void wirte2File(char * charList,int size,
+void wirte2File(char * charList, int size,
                 std::string fileName) {
-    
     std::ofstream outFile(fileName);
     int start = 0;
-    std::string str( &(charList[start]) );
-    outFile <<str<<'\n';
-    for(int i = 0; i < size;i++){
-        if(charList[i] == NULL) {
+    std::string str(&(charList[start]));
+    outFile << str << '\n';
+    for (int i = 0; i < size; i++) {
+        if (charList[i] == '\0') {
             start = i;
-            if( i != size-1) {
-            std::string str( &(charList[start+1]) );
-            outFile <<str<<'\n';
+            if (i != size-1) {
+            std::string str(&(charList[start+1]));
+            outFile << str << '\n';
             }
         }
     }
 }
-std::vector<int> func(std::vector<char> charList,cl::KernelFunctor& sort,
-                      std::vector<int> index, ClCmdQueue& queue) throw(cl::Error) {
+std::vector<int> func(std::vector<char> charList, cl::KernelFunctor& sort,
+                      std::vector<int> index, ClCmdQueue& queue)
+    throw(cl::Error) {
     std::vector<int> ret(index.size());
 
-    cl::Buffer charBuf = queue.makeBuffer(  const_cast<char *> (&charList[0]), charList.size(), queue.ROFlags);
-    cl::Buffer indexBuf = queue.makeBuffer(  const_cast<int *>(&index[0]),index.size()*sizeof(int),queue.ROFlags);
-
-    cl::Buffer retBuf = queue.makeBuffer( const_cast<int *> (&ret[0]),
-                                           index.size()*sizeof(int), queue.RWFlags);
+    cl::Buffer charBuf = queue.makeBuffer(const_cast<char *> (&charList[0]),
+                                              charList.size(), queue.ROFlags);
+    cl::Buffer indexBuf = queue.makeBuffer(const_cast<int *>(&index[0]),
+                                                 index.size()*sizeof(int),
+                                               queue.ROFlags);
+    cl::Buffer retBuf = queue.makeBuffer(const_cast<int *> (&ret[0]),
+                                           index.size()*sizeof(int),
+                                              queue.RWFlags);
     int size = index.size();
-
-    sort(charBuf,indexBuf,size,retBuf);
+    sort(charBuf, indexBuf, size, retBuf);
     queue.enqueueMapBuffer(retBuf, index.size()*sizeof(int));
-    
-
-    return ret;    
+    return ret;
 }
 int getDevType(const std::string& device) throw(std::runtime_error) {
     const std::string DevTypes = "CPUGPUACC";
@@ -94,11 +93,11 @@ std::vector<int> sortedIndex(std::vector<char> charList,
     ClCmdQueue queue(temp);
 
     cl::Kernel kernel   = queue.loadKernel("hw7.cl", "sort");
-    //const int blockSize = 8192;
+    // const int blockSize = 8192;
     int range = index.size();
     cl::KernelFunctor sort(kernel, queue.getQueue(), cl::NullRange,
                              cl::NDRange(range), cl::NullRange);
-    ret = func(charList,sort,index,queue);
+    ret = func(charList, sort, index, queue);
 
     return ret;
 }
@@ -111,11 +110,10 @@ int main(int argc, char *argv[]) {
     }
     std::vector<char> charList = readData(argv[2]);
     std::vector<int> index = indexCharList(charList);
-    char ret[charList.size()];
-    
+    int tempSize = charList.size();
+    char ret[tempSize];
     //    std::vector<int> index2={7,4,0};
-    std::vector<int> index2=sortedIndex(charList,index,argv[1]);
-    putStringRight(index,index2,charList,ret);
-    wirte2File(ret,charList.size(),argv[3]);
-
+    std::vector<int> index2 = sortedIndex(charList, index, argv[1]);
+    putStringRight(index, index2, charList, ret);
+    wirte2File(ret, charList.size(), argv[3]);
 }
